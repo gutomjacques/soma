259ffdb198f2e57db6453ed1ad2a27bc1626d90b
gutomjacques <guto.mjacques@gmail.com>
2025-02-25 20:49:05 -0300

    Add files via upload

---
+++ soma001.py
@@ -0,0 +1,20 @@
+"""
+Programa soma
+Descrição: Armazena os números 2 e 3, calcula a sua soma e imprime na tela o resultado.
+Autor: Augusto Jacques
+Data: 25/02/2025
+Versão: 0.0.1
+"""
+
+# Alocação de memória
+
+num_1 = 2
+num_2 = 3
+
+# processamento dos dados
+
+resultado = num_1 + num_2
+
+# saída de dados
+
+print(resultado)
